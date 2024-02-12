2a8d7766ebd6592c7bb506074e0554269fe6a03b
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-02-12 22:27:53 +0900

    fix: 순환 참조 오류 해결

--- module/infrastructure/build.gradle.kts
+++ module/infrastructure/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     implementation(project(":module:domain"))
     implementation(project(":module:application"))
-    implementation(project(":module:infrastructure"))
 
     implementation(spring.web)
     implementation(spring.data.jpa)
