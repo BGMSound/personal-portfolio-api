66820372c7e254e52a6f9d1c17c5d0d82fa7c45f
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-03-10 17:06:45 +0900

    refactor: LoginResponse 코드 정리

--- module/presentation/src/main/kotlin/kr/bgmsound/bgmlab/dto/response/LoginResponse.kt
+++ module/presentation/src/main/kotlin/kr/bgmsound/bgmlab/dto/response/LoginResponse.kt
@@ -1,9 +1,20 @@
 package kr.bgmsound.bgmlab.dto.response
 
+import kr.bgmsound.bgmlab.dto.LoggedInUserDto
 import kr.bgmsound.bgmlab.model.Role
 
 data class LoginResponse(
     val role: Role,
     val accessToken: String,
     val refreshToken: String
-)
+) {
+    companion object {
+        fun from(loggedInUser: LoggedInUserDto): LoginResponse {
+            return LoginResponse(
+                role = loggedInUser.getRepresentativeRole(),
+                accessToken = loggedInUser.accessToken,
+                refreshToken = loggedInUser.refreshToken
+            )
+        }
+    }
+}
