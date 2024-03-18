2c6598db3709cde577472c76127a94431523cfb0
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-03-18 14:44:10 +0900

    refactor: 소셜 로그인 & 네이티브 로그인 클래스 분리

--- module/application/src/main/kotlin/kr/bgmsound/bgmlab/service/impl/OAuthService.kt
+++ module/application/src/main/kotlin/kr/bgmsound/bgmlab/service/impl/OAuthService.kt
@@ -2,35 +2,39 @@ package kr.bgmsound.bgmlab.service.impl
 
 import kr.bgmsound.bgmlab.dto.LoggedInUserDto
 import kr.bgmsound.bgmlab.dto.SocialLoginResultDto
-import kr.bgmsound.bgmlab.dto.TokenDto
 import kr.bgmsound.bgmlab.error.exception.AuthenticationFailException
 import kr.bgmsound.bgmlab.model.User
 import kr.bgmsound.bgmlab.strategy.LoginProviderType
 import kr.bgmsound.bgmlab.adapter.authentication.LoginProviderManager
 import kr.bgmsound.bgmlab.adapter.authentication.TokenProvider
 import kr.bgmsound.bgmlab.adapter.account.UserSocialAccountRepository
+import kr.bgmsound.bgmlab.dto.AuthenticationDto
+import kr.bgmsound.bgmlab.dto.TokenDto
 import kr.bgmsound.bgmlab.dto.account.SocialAccount
 import kr.bgmsound.bgmlab.error.exception.UserNotFoundException
+import kr.bgmsound.bgmlab.model.TokenType
 import kr.bgmsound.bgmlab.repository.UserRepository
-import kr.bgmsound.bgmlab.service.AuthService
+import kr.bgmsound.bgmlab.repository.UserTokenRepository
+import kr.bgmsound.bgmlab.service.AbstractAuthService
 import kr.bgmsound.bgmlab.strategy.UserCreationStrategy
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class AuthServiceImpl(
-    private val loginProviderManager: LoginProviderManager,
-    private val tokenProvider: TokenProvider,
-    private val userCreationStrategy: UserCreationStrategy,
+class OAuthService(
+    tokenProvider: TokenProvider,
+    userTokenRepository: UserTokenRepository,
 
+    private val loginProviderManager: LoginProviderManager,
+    private val userCreationStrategy: UserCreationStrategy,
     private val userRepository: UserRepository,
     private val userSocialAccountRepository: UserSocialAccountRepository
-) : AuthService {
+) : AbstractAuthService(tokenProvider, userTokenRepository) {
 
     @Transactional
-    override fun socialLogin(type: LoginProviderType, code: String): LoggedInUserDto {
-        val provider = loginProviderManager.getSocialLoginProvider(type)
-        val loginResult = runCatching { provider.login(code) }.getOrElse { throw AuthenticationFailException() }
+    override fun login(authentication: AuthenticationDto): LoggedInUserDto {
+        val provider = loginProviderManager.getSocialLoginProvider(authentication.type)
+        val loginResult = runCatching { provider.login(authentication.credentials) }.getOrElse { throw AuthenticationFailException() }
 
         val socialUser = userSocialAccountRepository
             .findBySocialId(provider = loginResult.provider, socialId = loginResult.socialId)
@@ -39,15 +43,11 @@ class AuthServiceImpl(
             }
             ?: registerNewSocialUser(loginResult = loginResult)
 
-        val token = issueToken(socialUser)
-        return LoggedInUserDto.of(socialUser, token)
-    }
-
-    private fun issueToken(user: User): TokenDto {
-        return TokenDto.of(
-            accessToken = tokenProvider.createAccessToken(id = user.id, authorities = user.roles),
-            refreshToken = tokenProvider.createRefreshToken(id = user.id, authorities = user.roles)
+        val token = TokenDto.of(
+            issueNewToken(type = TokenType.ACCESS, user = socialUser),
+            issueNewToken(type = TokenType.REFRESH, user = socialUser)
         )
+        return LoggedInUserDto.of(socialUser, token)
     }
 
     private fun registerNewSocialUser(loginResult: SocialLoginResultDto): User {
@@ -61,5 +61,4 @@ class AuthServiceImpl(
         userSocialAccountRepository.save(account)
         return user
     }
-
 }
