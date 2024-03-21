934543916f4356c1edc14b24800a4f72729b0549
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-03-21 13:09:02 +0900

    refactor: 코드 클린업

--- module/application/src/main/kotlin/kr/bgmsound/bgmlab/authentication/OAuthAuthenticationStrategy.kt
+++ module/application/src/main/kotlin/kr/bgmsound/bgmlab/authentication/OAuthAuthenticationStrategy.kt
@@ -1,17 +1,19 @@
 package kr.bgmsound.bgmlab.authentication
 
+import kr.bgmsound.bgmlab.IdentifierGenerator
 import kr.bgmsound.bgmlab.authentication.dto.AuthenticationDto
 import kr.bgmsound.bgmlab.authentication.dto.OAuthResult
 import kr.bgmsound.bgmlab.authentication.dto.SocialAccount
+import kr.bgmsound.bgmlab.model.Role
 import kr.bgmsound.bgmlab.model.User
 import kr.bgmsound.bgmlab.repository.UserRepository
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class OAuthAuthenticationStrategy(
     private val oAuthGatewayFactory: OAuthGatewayFactory,
-    private val userCreationStrategy: UserCreationStrategy,
-
+    private val identifierGenerator: IdentifierGenerator,
     private val userRepository: UserRepository,
     private val userSocialAccountRepository: UserSocialAccountRepository
 ) : AuthenticationStrategy {
@@ -27,11 +29,23 @@ class OAuthAuthenticationStrategy(
     }
 
     private fun registerNewUser(loginResult: OAuthResult): User {
-        val user = userCreationStrategy.createNewUser(loginResult.provider, loginResult.socialId)
+        val user = createNewUser(loginResult.provider, loginResult.socialId)
         val account = SocialAccount.of(user = user, provider = loginResult.provider, socialId = loginResult.socialId)
 
         userRepository.save(user)
         userSocialAccountRepository.save(account)
         return user
+    }
+
+    private fun createNewUser(provider: LoginType, id: String): User {
+        val displayId = "${provider.name}$id".lowercase()
+
+        return User(
+            id = identifierGenerator.generateIdentifier(),
+            displayId = displayId,
+            name = displayId,
+            roles = listOf(Role.NEED_SIGNUP),
+            createAt = LocalDateTime.now()
+        )
     }
 }
