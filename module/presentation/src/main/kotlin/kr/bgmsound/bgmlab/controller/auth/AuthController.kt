1455f27a9a54fe3b3a7ba01b0be137f3cc8e5371
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-03-22 23:31:17 +0900

    feat: 리프레시 컨트롤러 추가

---
+++ module/presentation/src/main/kotlin/kr/bgmsound/bgmlab/controller/auth/AuthController.kt
@@ -0,0 +1,21 @@
+package kr.bgmsound.bgmlab.controller.auth
+
+import kr.bgmsound.bgmlab.authentication.service.AuthService
+import kr.bgmsound.bgmlab.dto.request.RefreshRequest
+import kr.bgmsound.bgmlab.model.Token
+import kr.bgmsound.bgmlab.model.TokenType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthController(
+    private val authService: AuthService,
+) {
+
+    @GetMapping("/refresh")
+    fun refresh(@RequestBody request: RefreshRequest) {
+        authService.refresh(refreshToken = Token(TokenType.REFRESH, request.refreshToken))
+    }
+
+}
---
+++ module/presentation/src/main/kotlin/kr/bgmsound/bgmlab/dto/request/RefreshRequest.kt
@@ -0,0 +1,5 @@
+package kr.bgmsound.bgmlab.dto.request
+
+data class RefreshRequest(
+    val refreshToken: String
+)
