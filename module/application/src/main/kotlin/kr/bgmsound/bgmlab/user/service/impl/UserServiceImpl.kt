79c3804b358a776cceeb2397436e2df4a4cb50e7
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2024-04-01 20:44:03 +0900

    feat: display id 변경 기능 추가

--- module/application/src/main/kotlin/kr/bgmsound/bgmlab/user/service/impl/UserServiceImpl.kt
+++ module/application/src/main/kotlin/kr/bgmsound/bgmlab/user/service/impl/UserServiceImpl.kt
@@ -1,18 +1,25 @@
 package kr.bgmsound.bgmlab.user.service.impl
 
+import kr.bgmsound.bgmlab.error.exception.UserAlreadyExistsException
 import kr.bgmsound.bgmlab.error.exception.UserNotFoundException
 import kr.bgmsound.bgmlab.model.User
 import kr.bgmsound.bgmlab.repository.UserRepository
 import kr.bgmsound.bgmlab.service.UserService
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository
 ) : UserService {
 
+    @Transactional
     override fun changeDisplayId(userId: String, displayId: String) {
-        TODO("Not yet implemented")
+        val user = userRepository.findById(userId) ?: throw UserNotFoundException()
+        if(userRepository.findByDisplayId(displayId) != null) {
+            throw UserAlreadyExistsException()
+        }
+        userRepository.save(user.copy(displayId = displayId))
     }
 
     override fun changeName(userId: String, name: String) {
--- module/domain/src/main/kotlin/kr/bgmsound/bgmlab/error/exception/UserException.kt
+++ module/domain/src/main/kotlin/kr/bgmsound/bgmlab/error/exception/UserException.kt
@@ -4,3 +4,5 @@ import kr.bgmsound.bgmlab.error.APIException
 import kr.bgmsound.bgmlab.error.ErrorCode
 
 class UserNotFoundException : APIException(ErrorCode.USER_NOT_FOUND)
+
+class UserAlreadyExistsException : APIException(ErrorCode.USER_ALREADY_EXISTS)
