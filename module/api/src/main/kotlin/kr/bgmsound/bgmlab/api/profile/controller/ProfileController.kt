c04d832761b9790228d4c09aa618318c36714e76
Gyumin Bae <bgmsound0528@gmail.com>
2024-05-24 22:21:59 +0900

    refactor: 코드 클린업

--- module/api/src/main/kotlin/kr/bgmsound/bgmlab/api/profile/controller/ProfileController.kt
+++ module/api/src/main/kotlin/kr/bgmsound/bgmlab/api/profile/controller/ProfileController.kt
@@ -4,7 +4,6 @@ import kr.bgmsound.bgmlab.api.profile.ProfileRequestMapper
 import kr.bgmsound.bgmlab.api.profile.dto.request.UpdateProfileRequest
 import kr.bgmsound.bgmlab.api.profile.dto.response.ProfileResponse
 import kr.bgmsound.bgmlab.application.profile.service.ProfileService
-import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
