ae5017451fbeeb456f77ec47e4f8c7aacce21075
Gyumin Bae <bgmsound0528@gmail.com>
2024-06-18 23:57:25 +0900

    refactor: 미사용 bean 제거

--- module/api/src/main/kotlin/kr/bgmsound/bgmlab/advice/security/config/WebSecurityConfig.kt
+++ module/api/src/main/kotlin/kr/bgmsound/bgmlab/advice/security/config/WebSecurityConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -54,10 +53,5 @@ class WebSecurityConfig(
                     .accessDeniedHandler(webAccessDeniedHandler)
             }
             .build()
-    }
-
-    @Bean
-    fun passwordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder()
     }
 }
