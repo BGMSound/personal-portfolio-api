793d1214cda4999512e61305fed33770c0ddaba2
Gyumin Bae <bgmsound0528@gmail.com>
2024-06-26 23:28:14 +0900

    refactor: profile 관련 기능 제거

--- module/persistence/src/main/kotlin/kr/bgmsound/bgmlab/persistence/repository/jpa/JpaSingletonProfileRepository.kt
+++
@@ -1,6 +0,0 @@
-package kr.bgmsound.bgmlab.persistence.repository.jpa
-
-import kr.bgmsound.bgmlab.persistence.entity.profile.SingletonProfileEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface JpaSingletonProfileRepository : JpaRepository<SingletonProfileEntity, Long>
